311703c9f8d6e0a34cedf7b7a703cdb3a24dd7b0
Inagdy <islam.m.nagdy@gmail.com>
2026-08-24 07:10:36 +0200

    adding SingleChildScrollView

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -11,69 +11,71 @@ class ShopScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.71),
-      child: Column(
-        children: [
-          SizedBox(height: 14.28),
-          Center(
-            child: Image.asset(
-              "assests/images/carrot.png",
-              width: 20.88,
-              height: 23.78,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 14.28),
+            Center(
+              child: Image.asset(
+                "assests/images/carrot.png",
+                width: 20.88,
+                height: 23.78,
+              ),
             ),
-          ),
-          SizedBox(height: 7.6),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Icon(Icons.location_on, color: Color(0xff4C4F4D)),
-              Text(
-                'Dhaka, Banassre',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight(600),
-                  color: Color(0xff4C4F4D),
+            SizedBox(height: 7.6),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+        
+              children: [
+                Icon(Icons.location_on, color: Color(0xff4C4F4D)),
+                Text(
+                  'Dhaka, Banassre',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight(600),
+                    color: Color(0xff4C4F4D),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          CustomTextField(searchFeild: true),
-          SizedBox(height: 20),
-          HomeSlider(),
-          SizedBox(height: 30),
-          HomeSectionTitle(leftWord: 'Exclusive Offer'),
-          SizedBox(height: 20),
-
-          Row(
-            children: [
-              
-              HomeBottom(
-                imagePath: "assests/images/banana.png",
-                mainTitle: "Organic Bananas",
-                secandTitle: "7pcs, Priceg",
-                price: "\$4.99",
-              ),
-              SizedBox(width: 15.07),
-              HomeBottom(
-                imagePath: "assests/images/apple.png",
-                mainTitle: "Red Apple",
-                secandTitle: "1kg, Priceg",
-                price: "\$4.99",
-              ),
-            ],
-          ),
-          SizedBox(height: 30,),
-          HomeSectionTitle(leftWord: "Best Selling"),
-          SizedBox(height: 20.28,),
-          Row(
-            children: [
-              HomeBottom(imagePath: "assests/images/redChili.png", mainTitle: "Bell Pepper Red", secandTitle: "1kg, Priceg", price:"\$4.99" ),
-              SizedBox(width: 15.07),
-              HomeBottom(imagePath: "assests/images/ginger.png", mainTitle: "Ginger", secandTitle: "250gm, Priceg", price: "\$4.99"),
-            ],
-          )
-        ],
+              ],
+            ),
+            SizedBox(height: 20),
+            CustomTextField(searchFeild: true),
+            SizedBox(height: 20),
+            HomeSlider(),
+            SizedBox(height: 30),
+            HomeSectionTitle(leftWord: 'Exclusive Offer'),
+            SizedBox(height: 20),
+        
+            Row(
+              children: [
+                
+                HomeBottom(
+                  imagePath: "assests/images/banana.png",
+                  mainTitle: "Organic Bananas",
+                  secandTitle: "7pcs, Priceg",
+                  price: "\$4.99",
+                ),
+                SizedBox(width: 15.07),
+                HomeBottom(
+                  imagePath: "assests/images/apple.png",
+                  mainTitle: "Red Apple",
+                  secandTitle: "1kg, Priceg",
+                  price: "\$4.99",
+                ),
+              ],
+            ),
+            SizedBox(height: 30,),
+            HomeSectionTitle(leftWord: "Best Selling"),
+            SizedBox(height: 20.28,),
+            Row(
+              children: [
+                HomeBottom(imagePath: "assests/images/redChili.png", mainTitle: "Bell Pepper Red", secandTitle: "1kg, Priceg", price:"\$4.99" ),
+                SizedBox(width: 15.07),
+                HomeBottom(imagePath: "assests/images/ginger.png", mainTitle: "Ginger", secandTitle: "250gm, Priceg", price: "\$4.99"),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
