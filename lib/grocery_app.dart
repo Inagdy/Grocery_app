ef76c1d1f6fe24ac4fd16f2ddef7c434324dba10
Inagdy <islam.m.nagdy@gmail.com>
2026-08-30 01:43:00 +0200

    adding material widget to shopscreen as per issue shown  in text underline with yellow alert

--- lib/grocery_app.dart
+++ lib/grocery_app.dart
@@ -16,7 +16,7 @@ class GroceryApp extends StatelessWidget {
   Widget build(BuildContext context) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home:const ProductDetails(),
+        home:const ShopScreen(),
       );
    }
 }
--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -12,104 +12,106 @@ class ShopScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.71),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 14.28),
-            Center(
-              child: Image.asset(
-                "assests/images/carrot.png",
-                width: 20.88,
-                height: 23.78,
-              ),
-            ),
-            SizedBox(height: 7.6),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-
-              children: [
-                Icon(Icons.location_on, color: Color(0xff4C4F4D)),
-                Text(
-                  'Dhaka, Banassre',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight(600),
-                    color: Color(0xff4C4F4D),
-                  ),
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.71),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 14.28),
+              Center(
+                child: Image.asset(
+                  "assests/images/carrot.png",
+                  width: 20.88,
+                  height: 23.78,
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            CustomTextField(searchFeild: true),
-            SizedBox(height: 20),
-            HomeSlider(),
-            SizedBox(height: 30),
-            HomeSectionTitle(leftWord: 'Exclusive Offer'),
-            SizedBox(height: 20),
-
-            SizedBox(
-              height: 248.51,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: products.length,
-                separatorBuilder: (context, index) =>
-                    const SizedBox(width: 15.07),
-                itemBuilder: (context, index) {
-                  return HomeBottom(productModel: products[index]);
-                },
               ),
-            ),
-            SizedBox(height: 30),
-            HomeSectionTitle(leftWord: "Best Selling"),
-            SizedBox(height: 20.28),
-            SizedBox(
-              height: 248.51,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: products.length,
-                separatorBuilder: (context, index) =>
-                    const SizedBox(width: 15.07),
-                itemBuilder: (context, index) {
-                  return HomeBottom(productModel: products[index]);
-                },
+              SizedBox(height: 7.6),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+      
+                children: [
+                  Icon(Icons.location_on, color: Color(0xff4C4F4D)),
+                  Text(
+                    'Dhaka, Banassre',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight(600),
+                      color: Color(0xff4C4F4D),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 30),
-            HomeSectionTitle(leftWord: "Groceries"),
-            SizedBox(height: 20),
-
-            Padding(
-              padding: const EdgeInsets.only(left: 24.71),
-              child: SizedBox(
-                height: 105,
+              SizedBox(height: 20),
+              CustomTextField(searchFeild: true),
+              SizedBox(height: 20),
+              HomeSlider(),
+              SizedBox(height: 30),
+              HomeSectionTitle(leftWord: 'Exclusive Offer'),
+              SizedBox(height: 20),
+      
+              SizedBox(
+                height: 248.51,
                 child: ListView.separated(
                   scrollDirection: Axis.horizontal,
-                  itemCount: groceries.length,
+                  itemCount: products.length,
                   separatorBuilder: (context, index) =>
-                      const SizedBox(width: 14.95),
+                      const SizedBox(width: 15.07),
                   itemBuilder: (context, index) {
-                    return SecandHomeSlider(groceriesModel: groceries[index]);
+                    return HomeBottom(productModel: products[index]);
                   },
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-
-            SizedBox(
-              height: 248.51,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: products.length,
-                separatorBuilder: (context, index) =>
-                    const SizedBox(width: 15.07),
-                itemBuilder: (context, index) {
-                  return HomeBottom(productModel: products[index]);
-                },
+              SizedBox(height: 30),
+              HomeSectionTitle(leftWord: "Best Selling"),
+              SizedBox(height: 20.28),
+              SizedBox(
+                height: 248.51,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: products.length,
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(width: 15.07),
+                  itemBuilder: (context, index) {
+                    return HomeBottom(productModel: products[index]);
+                  },
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+              HomeSectionTitle(leftWord: "Groceries"),
+              SizedBox(height: 20),
+      
+              Padding(
+                padding: const EdgeInsets.only(left: 24.71),
+                child: SizedBox(
+                  height: 105,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: groceries.length,
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(width: 14.95),
+                    itemBuilder: (context, index) {
+                      return SecandHomeSlider(groceriesModel: groceries[index]);
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+      
+              SizedBox(
+                height: 248.51,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: products.length,
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(width: 15.07),
+                  itemBuilder: (context, index) {
+                    return HomeBottom(productModel: products[index]);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
