3df32a075986f85227a940cd775ba486fa69952b
Inagdy <islam.m.nagdy@gmail.com>
2026-09-06 07:03:39 +0200

    splash screen

--- lib/grocery_app.dart
+++ lib/grocery_app.dart
@@ -1,15 +1,8 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:grocery_app/screens/account_screen.dart';
 import 'package:grocery_app/screens/botton_nav_bar_screen.dart';
-import 'package:grocery_app/screens/cart_screen.dart';
-import 'package:grocery_app/screens/login_screen.dart';
-import 'package:grocery_app/screens/product_details.dart';
-import 'package:grocery_app/screens/shop_screen.dart';
-import 'package:grocery_app/screens/signup_screen.dart';
 
-import 'screens/shop_screen.dart';
 
 
 class GroceryApp extends StatelessWidget {
